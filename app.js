33c7a85caa94eaaec8468ef8dae4d8ae938585f0
John LaDeur <jdladeur@gmail.com>
2019-11-10 14:16:31 -0600

    if statements for digital layout, added symbols to clock

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+function clock (){
 var fullDate = new Date ();
 var hours = fullDate.getHours();
 var minutes =fullDate.getMinutes();
@@ -7,10 +8,34 @@ var day = fullDate.getDay();
 var month= fullDate.getMonth();
 var year = fullDate.getFullYear();
 
+if (hours < 10) {
+    hours = "0" + hours;
+}
+
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+
+if (seconds < 10) {
+    seconds = "0" + seconds;
+}
+
+if (month < 10) {
+    month = "0" + month;
+}
+
+if (day < 10) {
+    day = "0" + day;
+}
+
 document.getElementById('hour').innerHTML = hours;
-document.getElementById('minute').innerHTML = minutes;
-document.getElementById('second').innerHTML = seconds;
+document.getElementById('minute').innerHTML = ":" + minutes;
+document.getElementById('second').innerHTML = ":" + seconds;
 
 document.getElementById('month').innerHTML = month;
-document.getElementById('day').innerHTML = day;
-document.getElementById('year').innerHTML = year;
+document.getElementById('day').innerHTML = "/" + day;
+document.getElementById('year').innerHTML = "/" + year;
+
+}
+
+setInterval(clock, 1000);
