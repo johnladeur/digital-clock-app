e1d70d9e8030e3a7a507bf90f31ea9e4c45e1fa3
John LaDeur <jdladeur@gmail.com>
2019-11-11 19:41:57 -0600

    added function and comments for click event

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var seconds = fullDate.getSeconds();
 var day = fullDate.getDate();
 var month= fullDate.getMonth();
 var year = fullDate.getFullYear();
-
+var change = true;
 
 if (hours < 10) {
     hours = "0" + hours;
@@ -37,6 +37,22 @@ document.getElementById('month').innerHTML = month + 1;
 document.getElementById('day').innerHTML = "/" + day;
 document.getElementById('year').innerHTML = "/" + year;
 
+//if (clickEvent) --> switch value of var change to false
+//if change == false && hours > 12 --> return hours = hours - 12;
+//else {do nothing}
+
+document.getElementById('myButton').addEventListener("click", formatChange);
+
+
+function formatChange(){
+    if (hours > 12){
+        document.getElementById('myButton').innerHTML = hours -= 12;
+}   
+
 }
 
+
 setInterval(clock, 1000);
+
+
+}
