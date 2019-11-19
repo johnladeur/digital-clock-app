8adf2f40b8f88460f103a4df07c6bc2f9ae55b71
John LaDeur <jdladeur@gmail.com>
2019-11-19 13:40:03 -0600

    reduced lines of code - replaced multiple if statements with formatDateTimeNumber function

--- app.js
+++ app.js
@@ -1,59 +1,38 @@
+(function() {
+  function clock() {
+    var fullDate = new Date();
+    var hours = formatDateTimeNumber(fullDate.getHours());
+    var minutes = formatDateTimeNumber(fullDate.getMinutes());
+    var seconds = formatDateTimeNumber(fullDate.getSeconds());
+    var day = formatDateTimeNumber(fullDate.getDate());
+    var month = formatDateTimeNumber(fullDate.getMonth());
+    var year = formatDateTimeNumber(fullDate.getFullYear());
 
-
-function clock (){
-    var fullDate = new Date ();
-    var hours = fullDate.getHours();
-    var minutes =fullDate.getMinutes();
-    var seconds = fullDate.getSeconds();
-    var day = fullDate.getDate();
-    var month= fullDate.getMonth();
-    var year = fullDate.getFullYear();  
-      
-      
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    
-    if (month < 10) {
-        month = "0" + month;
-    }
-    
-    if (day < 10) {
-        day = "0" + day;
-    }
-    
     if (is24hr == false && hours > 12) {
-        hours = hours - 12;
+      hours = hours - 12;
     }
 
-document.getElementById('hour').innerHTML = hours;
-document.getElementById('minute').innerHTML = ":" + minutes;
-document.getElementById('second').innerHTML = ":" + seconds;
-    
-document.getElementById('month').innerHTML = month + 1;
-document.getElementById('day').innerHTML = "/" + day;
-document.getElementById('year').innerHTML = "/" + year;
-    
-}
-var is24hr = true;
-var timeButton = document.getElementById('myButton');
-timeButton.addEventListener("click", function() {
-        is24hr = !is24hr;
-        //if (is24hr == true){
-          //  is24hr = false
-          //} else {
-            //is24hr = true
-          //}   
-});
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = ":" + minutes;
+    document.getElementById("second").innerHTML = ":" + seconds;
 
-  
-setInterval(clock, 1000); 
-    
+    document.getElementById("month").innerHTML = month + 1;
+    document.getElementById("day").innerHTML = "/" + day;
+    document.getElementById("year").innerHTML = "/" + year;
+  }
+
+  function formatDateTimeNumber(dateTimeNumber) {
+    if (dateTimeNumber < 10) {
+      return "0" + dateTimeNumber;
+    }
+    return dateTimeNumber;
+  }
+
+  var is24hr = true;
+  var timeButton = document.getElementById("myButton");
+  timeButton.addEventListener("click", function() {
+    is24hr = !is24hr;
+  });
+
+  setInterval(clock, 1000);
+})();
